406b3fe5788a43a2f32696417ef66eb9e6c9d54c
Thomas Ruiz <truizw@gmail.com>
2023-11-23 17:58:18 -0500

    Code non commenté pour la cinématique directe

---
+++ cinematique_directe.py
@@ -0,0 +1,80 @@
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+from time import *
+
+def Affichage_configuration(t1,t2,t3,t4,t5,T01,T12,T23,T34,T45,l1,l2,l3,l4):
+
+    T01_eval = T01.subs([(theta1,t1),(L1,l1)])
+    T12_eval = T12.subs([(theta2,t2)])
+    T23_eval = T23.subs([(theta3,t3),(L2,l2)])
+    T34_eval = T34.subs([(theta4,t4),(L3,l3)])
+    T45_eval = T45.subs([(theta5,t5)])
+
+    T05_eval = T01_eval*T12_eval*T23_eval*T34_eval*T45_eval
+    coord_O = [0,0,0]
+    coord_A = [0,0,l1]
+    
+    coord_B = (T01_eval*T12_eval)*sp.Matrix([0,l2,0,1])
+    coord_B = coord_B[0:3]
+
+    coord_C = (T01_eval*T12_eval*T23_eval)*sp.Matrix([0,l3,0,1])
+    coord_C = coord_C[0:3]
+
+    coord_D = T05_eval*sp.Matrix([0,l4,0,1])
+    coord_D = coord_D[0:3]
+
+    Segment_OA = [[coord_O[0],coord_A[0]],[coord_O[1],coord_A[1]],[coord_O[2],coord_A[2]]]
+    Segment_AB = [[coord_A[0],coord_B[0]],[coord_A[1],coord_B[1]],[coord_A[2],coord_B[2]]]
+    Segment_BC = [[coord_B[0],coord_C[0]],[coord_B[1],coord_C[1]],[coord_B[2],coord_C[2]]]
+    Segment_CD = [[coord_C[0],coord_D[0]],[coord_C[1],coord_D[1]],[coord_C[2],coord_D[2]]]
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.set_title('Configuration du robot')
+    ax.set_xlabel('Axe X')
+    ax.set_ylabel('Axe Y')
+    ax.set_zlabel('Axe Z')
+    ax.plot(Segment_OA[0], Segment_OA[1], Segment_OA[2], 'red')
+    ax.plot(Segment_AB[0], Segment_AB[1], Segment_AB[2], 'green')
+    ax.plot(Segment_BC[0], Segment_BC[1], Segment_BC[2], 'orange')
+    ax.plot(Segment_CD[0], Segment_CD[1], Segment_CD[2], 'purple')
+    plt.show()
+
+
+
+theta1,theta2,theta3,theta4,theta5,L1,L2,L3,L4 = sp.symbols('theta1,theta2,theta3,theta4,theta5,L1,L2,L3,L4',positive=True)
+
+T01 = sp.Matrix([[sp.cos(theta1),-sp.sin(theta1),0,0],[sp.sin(theta1),sp.cos(theta1),0,0],[0,0,1,L1],[0,0,0,1]])
+T12 = sp.Matrix([[1,0,0,0],[0,sp.cos(theta2),-sp.sin(theta2),0],[0,sp.sin(theta2),sp.cos(theta2),0],[0,0,0,1]])
+T23 = sp.Matrix([[1,0,0,0],[0,sp.cos(theta3),-sp.sin(theta3),L2],[0,sp.sin(theta3),sp.cos(theta3),0],[0,0,0,1]])
+T34 = sp.Matrix([[sp.cos(theta4),0,sp.sin(theta4),0],[0,1,0,L3],[-sp.sin(theta4),0,sp.cos(theta4),0],[0,0,0,1]])
+T45 = sp.Matrix([[1,0,0,0],[0,sp.cos(theta5),-sp.sin(theta5),0],[0,sp.sin(theta5),sp.cos(theta5),0],[0,0,0,1]])
+
+T05 = T01*T12*T23*T34*T45
+
+p_final = T05[0:3,3]
+R05 = T01[0:3,0:3]*T12[0:3,0:3]*T23[0:3,0:3]*T34[0:3,0:3]*T45[0:3,0:3]
+
+Affichage_configuration(0,0,0,0,0,T01,T12,T23,T34,T45,50,150,150,50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
